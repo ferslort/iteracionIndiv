a55efa9bb2485669e558d4e092d4f14dcf1cee9e
Fernando Padilla <fernandoslort@gmail.com>
2022-06-17 20:07:05 -0400

    update:   users/views.py

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 
-from .models import UserCustom
+from .models import Cliente
+from .forms import RegisterForm
 
 
 def user_page(request):
@@ -10,8 +12,39 @@ def user_page(request):
 
 def index_page(request):
 
-    users = UserCustom.objects.all()
+    users = Cliente.objects.all()
 
     return render(request, 'index.html', {
         'users': users
     })
+
+
+def register_page(request):
+
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        name = request.POST['name']
+        lastName = request.POST['lastName']
+        address = request.POST['address']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        client = Cliente(
+            name = name,
+            lastName = lastName,
+            address=address,
+            phone=phone,
+            email=email,
+            password = password
+        )
+
+        client.save()
+        messages.success(request, 'Registro Exitoso')
+
+
+    return render(request, 'register.html', {
+        'form': form
+    })
+
