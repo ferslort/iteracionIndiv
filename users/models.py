d468514f4d026ddff829e5089127a2039cdf0a50
Fernando Padilla <fernandoslort@gmail.com>
2022-06-17 20:06:43 -0400

    update:users/models.py

--- users/models.py
+++ users/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class UserCustom(models.Model):
+class Cliente(models.Model):
     name = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=50)
