53d701062721171d8431fa94873be4a8ee1413d4
Fernando Padilla <fernandoslort@gmail.com>
2022-06-20 12:27:43 -0400

    update: users/forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,31 +1,52 @@
 from django import forms
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+
+class FomRegister (UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email',
+                  'username', 'password1', 'password2']
+
+
+class FormLogin (forms.Form):
+
+    username = forms.CharField(
+        label='Usuario',
+        required=True
+    )
+    password = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput()
+    )
 
 
 class RegisterForm (forms.Form):
 
     name = forms.CharField(
-      label='Nombre',
-       required = True
+        label='Nombre',
+        required=True
     )
     lastName = forms.CharField(
-      label= 'Apellido',
-       required = True
+        label='Apellido',
+        required=True
     )
     address = forms.CharField(
-     label= 'Dirección',
-       required = True
+        label='Dirección',
+        required=True
     )
     phone = forms.CharField(
-      label= 'Teléfono',
-       required = True
+        label='Teléfono',
+        required=True
     )
     email = forms.EmailField(
-        label= 'Email',
-       required = True
+        label='Email',
+        required=True
     )
     password = forms.CharField(
-      label= 'Contraseña',
-       required = True
+        label='Contraseña',
+        required=True
     )
