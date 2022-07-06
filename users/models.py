670f70cc689d8b097443a0da53c96cf9208fe595
Fernando Padilla <fernandoslort@gmail.com>
2022-07-06 08:57:16 -0400

    update: users/models.py

--- users/models.py
+++ users/models.py
@@ -4,8 +4,5 @@ from django.db import models
 
 class Cliente(models.Model):
     name = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=50)
