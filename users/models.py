d2db088a6a7dabc0369c692f1bbb059e9801de9f
Fernando Padilla <fernandoslort@gmail.com>
2022-07-07 15:32:26 -0400

    update:users/models.py

--- users/models.py
+++ users/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 
 class User(AbstractUser):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
-    password = models.CharField(max_length=50)
+    password = models.CharField(max_length=200)
